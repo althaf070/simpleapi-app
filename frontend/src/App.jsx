d39e2e58fe803709633bc659e82aed9963f6e105
Muhammed Althaf <Mdalthaf424@gmail.com>
2024-08-04 10:35:15 +0530

    added link

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [datas, setDatas] = useState([])
 
   const fetchProducts = async() =>{
-const res = await fetch("http://localhost:5000/api/products")
+const res = await fetch("https://simpleapi-app.onrender.com/api/products")
 const data = await res.json()
 setDatas(data)
   }
--- frontend/src/assets/ProductsForm.jsx
+++ frontend/src/assets/ProductsForm.jsx
@@ -11,7 +11,7 @@ const ProductForm = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post('http://localhost:5000/api/products', {
+            const response = await axios.post('https://simpleapi-app.onrender.com/api/products', {
                 name,
                 quantity,
                 price,
